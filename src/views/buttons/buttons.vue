d866ebd9df9172c4d58cccd3a3d7eba7fc405299
zhaodengping <1356934076@qq.com>
2019-10-20 16:24:47 +0800

    基础/朴素/圆角按钮

--- src/views/buttons/buttons.vue
+++ src/views/buttons/buttons.vue
@@ -1,5 +1,5 @@
 <template>
-    <button :type="activeType" :class="[type?'u-button-'+type:'','u-button']">
+    <button :type="activeType" :class="[type?'u-button-'+type:'','u-button',{'is-plain':plain,'is-cirlce':circle}]">
         <slot></slot>
     </button>
 </template>
@@ -9,8 +9,18 @@ export default {
         type:{
             type:String,
             default:'default'
-        }
-    }
+        },
+        activeType:{
+            type:String,
+            default:'button'
+        },
+        plain:{
+            type:Boolean,
+        },
+        circle:{
+            type:Boolean
+        },        
+    },
 }
 </script>
 <style lang="scss" scoped>
@@ -18,10 +28,133 @@ export default {
     border-width:0;
     padding: 12px 20px;
     border-radius: var(--border-radius);
+    outline: none;
+    cursor: pointer;
+    font-weight: 500;
+    line-height: 1;
+}
+.u-button-default{
+    border:1px solid #dcdfe6;
+    color: var(--default-color);
+    &:hover,&:focus{
+        background-color: var(--default-focus);
+        border-color: var(--theme-focus);
+        color: var(--theme-color)
+    };
+}
+.u-button-default.is-plain{
+    &:hover,&:focus{
+        color: var(--theme-color);
+        border-color: var(--theme-color);
+        background-color: #fff;
+    }
+}
+.u-button-default.is-cirlce{
+    border-radius: 20px;
 }
 .u-button-primary{
     background-color: var(--theme-color);
     color: #ffffff;
+    &:hover,&:focus{
+        background-color: var(--theme-focus);
+        border-color: var(--theme-focus);
+    }
 }
-
+.u-button-primary.is-plain{
+    border:1px solid var(--theme-focus);
+    background-color: var(--default-focus);
+    color: var(--theme-color);
+    &:hover,&:focus{
+        border-color: var(--theme-focus);
+        background-color: var(--theme-focus);
+        color: #fff;
+    }
+}
+.u-button-primary.is-cirlce{
+    border-radius: 20px;
+}
+.u-button-success{
+    background-color: var(--success-color);
+    color: #fff;
+    &:hover,&:focus{
+        background-color: var(--success-focus);
+        border-color: var(--success-focus);
+    }
+}
+.u-button-success.is-plain{
+    background-color: #f0f9eb;
+    color: var(--success-color);
+    border:1px solid var(--success-color);
+    &:hover,&:focus{
+        background-color: var(--success-color);
+        border-color: var(--success-focus);
+        color: #fff;
+    }
+}
+.u-button-success.is-cirlce{
+    border-radius: 20px;
+}
+.u-button-info{
+    background-color: var(--info-color);
+    color: #fff;
+    &:hover,&:focus{
+        background-color: var(--info-focus);
+        border-color: var(--info-focus);
+    }
+}
+.u-button-info.is-plain{
+    background-color: #f0f9eb;
+    color: var(--success-color);
+    border:1px solid var(--success-color);
+    &:hover,&:focus{
+        background-color: var(--success-color);
+        border-color: var(--success-focus);
+        color: #fff;
+    }
+}
+.u-button-info.is-cirlce{
+    border-radius: 20px;
+}
+.u-button-warning{
+    background-color: var(--warning-color);
+    color: #fff;
+    &:hover,&:focus{
+        background-color: var(--warning-focus);
+        border-color: var(--warning-focus);
+    }
+}
+.u-button-warning.is-plain{
+    background-color: #fdf6ec;
+    color: var(--warning-color);
+    border:1px solid var(--warning-color);
+    &:hover,&:focus{
+        background-color: var(--warning-color);
+        border-color: var(--warning-focus);
+        color: #fff;
+    }
+}
+.u-button-warning.is-cirlce{
+    border-radius: 20px;
+}
+.u-button-danger{
+    background-color: var(--danger-color);
+    color: #fff;
+    &:hover,&:focus{
+        background-color: var(--danger-focus);
+        border-color: var(--danger-focus);
+    }
+}
+.u-button-danger.is-plain{
+    background-color: #fef0f0;
+    color: var(--danger-color);
+    border:1px solid var(--danger-color);
+    &:hover,&:focus{
+        background-color: var(--danger-color);
+        border-color: var(--danger-focus);
+        color: #fff;
+    }
+}
+.u-button-danger.is-cirlce{
+    border-radius: 20px;
+}
 </style>
--- src/views/buttons/uButton.vue
+++ src/views/buttons/uButton.vue
@@ -1,5 +1,27 @@
 <template>
-    <u-button type='primary'>主要按钮</u-button>
+    <div class="btn">
+        <h2>基础按钮</h2>
+        <u-button>默认按钮</u-button>
+        <u-button type='primary'>主要按钮</u-button>
+        <u-button type='success'>成功按钮</u-button>
+        <u-button type='info'>信息按钮</u-button>
+        <u-button type='warning'>警告按钮</u-button>
+        <u-button type='danger'>危险按钮</u-button>
+        <h2>朴素按钮</h2>
+        <u-button plain>朴素按钮</u-button>
+        <u-button type='primary' plain>主要按钮</u-button>
+        <u-button type='success' plain>成功按钮</u-button>
+        <u-button type='info' plain>信息按钮</u-button>
+        <u-button type='warning' plain>警告按钮</u-button>
+        <u-button type='danger' plain>危险按钮</u-button>
+        <h2>圆角按钮</h2>
+        <u-button circle>圆角按钮</u-button>
+        <u-button type='primary' circle>主要按钮</u-button>
+        <u-button type='success' circle>成功按钮</u-button>
+        <u-button type='info' circle>信息按钮</u-button>
+        <u-button type='warning' circle>警告按钮</u-button>
+        <u-button type='danger' circle>危险按钮</u-button>
+    </div>
 </template>
 <script>
 import uButton from './buttons.vue'
@@ -12,3 +34,8 @@ export default {
     }
 }
 </script>
+<style lang="scss" scoped>
+.btn{
+    margin: 20px;
+}
+</style>
