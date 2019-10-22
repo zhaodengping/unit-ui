65313c9600b3b4cf76b53caa4ddd366e8361775d
Alice <1356934076@qq.com>
2019-10-22 18:29:29 +0800

    grid

--- src/uses/uLayout.vue
+++ src/uses/uLayout.vue
@@ -1,3 +1,25 @@
 <template>
-    <div>布局</div>
+    <div>
+        <h2>基础布局</h2>
+        <u-grid :span='24'>
+            <div class="number">1</div>
+        </u-grid>
+        <u-grid :span='12'>
+            <div class="number">1</div>
+            <div class="number">2</div>
+        </u-grid>
+    </div>
 </template>
+<script>
+import uGrid from '../views/layout/layout.vue'
+export default {
+    components:{
+        uGrid
+    }
+}
+</script>
+<style lang="scss" scoped>
+.number{
+    background-color: var(--theme-color)
+}
+</style>
---
+++ src/views/layout/layout.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="grid" :style="{gridTemplateColumns:columnWidth}">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        span:{
+            type:Number,
+        }
+    },
+    computed:{
+        columnWidth:function(){
+            return 100/24 * this.span
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+.grid{
+    display: grid;
+    
+    width: 100%;
+}
+</style>
