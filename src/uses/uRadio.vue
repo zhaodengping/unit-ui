99f4570f3032c602aaa1102f6098a9b1c033847e
Alice <1356934076@qq.com>
2019-10-25 17:15:00 +0800

    添加样式

--- src/uses/uRadio.vue
+++ src/uses/uRadio.vue
@@ -1,5 +1,8 @@
 <template>
     <div>
-        按钮
+        <h1>单选框</h1>
+        <h2>基础按钮</h2>
+        <u-radio>选项1</u-radio>
+        <u-radio>选项2</u-radio>
     </div>
 </template>
--- src/views/radio/components/radio.vue
+++ src/views/radio/components/radio.vue
@@ -1,10 +1,45 @@
 <template>
-    <div>
-        
-    </div>
+    <label class="u-radio">
+        <span class="u-radio_input">
+            <div class="u-radio_inner"></div>
+            <input type="radio" class="u-radio_original">
+        </span>
+        <span class="u-radio-label">
+            <slot></slot>
+        </span>
+    </label>
 </template>
 <script>
 export default {
     name:'uRadio'
 }
 </script>
+<style lang="scss" scoped>
+    .u-radio{
+        display: flex;;
+        cursor: pointer;
+        color: #606266;
+        .u-radio_input{
+            display: inline-block;
+            .u-radio_inner{
+                position: relative;
+                width: 14px;
+                height: 14px;
+                border: 1px solid #606266;
+                border-radius: 100%;
+                &::after{
+                    width: 4px;
+                    height: 4px;
+                    content: ''
+                }
+            };
+            .u-radio_original{
+                position: absolute;
+                opacity: 0;
+                z-index: -1;
+                outline: none
+            }
+        }
+        
+    }
+</style>
