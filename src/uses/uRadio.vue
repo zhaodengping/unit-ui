de502dacb3d808ff99599f6856818c3ac0df42b9
Alice <1356934076@qq.com>
2019-10-28 17:59:08 +0800

    基础按钮实现点击

--- src/uses/uRadio.vue
+++ src/uses/uRadio.vue
@@ -2,7 +2,16 @@
     <div>
         <h1>单选框</h1>
         <h2>基础按钮</h2>
-        <u-radio>选项1</u-radio>
-        <u-radio>选项2</u-radio>
+        <u-radio v-model="value" label='1'>选项1</u-radio>
+        <u-radio v-model="value" label='2'>选项2</u-radio>
     </div>
 </template>
+<script>
+export default {
+    data(){
+        return{
+            value:'1',
+        }
+    }
+}
+</script>
