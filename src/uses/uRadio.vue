c8527cd358b1e2fa6ba4a6ebfc040aa6956b14fd
Alice <1356934076@qq.com>
2019-10-29 14:00:35 +0800

    单选框基础组件完成

--- src/uses/uRadio.vue
+++ src/uses/uRadio.vue
@@ -1,9 +1,13 @@
 <template>
     <div>
         <h1>单选框</h1>
-        <h2>基础按钮</h2>
+        <h2>基础单选框</h2>
         <u-radio v-model="value" label='1'>选项1</u-radio>
         <u-radio v-model="value" label='2'>选项2</u-radio>
+        <div class="choose">选择的value值为{{value}}</div>
+        <h2>禁用单选框</h2>
+        <u-radio v-model='value' disabled>禁用1</u-radio>
+        <u-radio v-model='value' label='1' disabled>禁用2</u-radio>
     </div>
 </template>
 <script>
@@ -12,6 +16,11 @@ export default {
         return{
             value:'1',
         }
-    }
+    },
 }
 </script>
+<style lang="scss" scoped>
+    .choose{
+        margin: 10px 0;
+    }
+</style>
