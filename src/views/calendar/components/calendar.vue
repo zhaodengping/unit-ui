e3220f53f17b9702981fdf7ac8d0d3fbbaaecb6d
Alice <1356934076@qq.com>
2019-10-30 17:15:00 +0800

    对时间做补充

--- src/views/calendar/components/calendar.vue
+++ src/views/calendar/components/calendar.vue
@@ -18,8 +18,11 @@
             </div>
             <table>
                 <tbody>
-                    <tr class="tr">
+                    <tr class="week">
                         <th v-for='(item,index) in week' :key="index">{{item}}</th>
+                    </tr>
+                    <tr class="day">
+                        <th v-for='(item,index) in day' :key="index" :class="[item==today.day?'is-today':'']">{{item}}</th>
                     </tr>
                 </tbody>
             </table>
@@ -34,12 +37,74 @@ export default {
     data(){
         return{
             week:week,
-            dialogShow:false
+            dialogShow:false,
+            day:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],
+            today:{
+                year:0,
+                month:0,
+                day:0
+            },
+            isWeek:-1,//每月第一天是周几
         }
     },
+    created(){
+        this.getDays()
+    },
     methods: {
+        //是否是闰年,年份能被4整除，但是不能是整数，或者能被400整除
+        isDouble(data){
+            const cond1=data%4==0;
+            const cond2=data%100!==0;
+            const cond3=data%400==0
+            if(cond1&&cond2||cond3){
+                return true
+            }
+            return false
+        },
+        //每个月的天数
+        getDays(){
+            let date=new Date();
+            let year=date.getFullYear();
+            let month=date.getMonth()+1;
+            let isDouble=this.isDouble(year)
+            let day=date.getDate();
+            this.whichIsFirstDay(year,month)
+            this.today={
+                year,
+                month,
+                day:day
+            }
+            switch(month){
+                case 2:{
+                    if(isDouble){
+                        this.day.pop()
+                    }else{
+                        this.day.splice(-2,2)
+                    }
+                    break;
+                };
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:{
+                    this.day.push(31);
+                    break;
+                };
+            }
+        },
+        //判断传入的年份的月份第一天是周几
+        whichIsFirstDay(year,month){
+            let number=new Date()
+            console.log(number)
+        },
         focus(){
             this.dialogShow=true;
+        },
+        blur(){
+            this.dialogShow=false
         },
     },
 }
@@ -49,7 +114,7 @@ export default {
         position: relative;
         .input-pre{
             position: absolute;
-            left: 2px;
+            left: 5px;
             top: 2px;
             color: #c0c4cc
         }
@@ -61,6 +126,7 @@ export default {
             border-style: solid;
             border-color:#dcdfe6;
             color: transparent;
+            width: 220px;
         }
     }
     .dialog{
@@ -71,7 +137,7 @@ export default {
         border: 1px solid #dcdfe6;
         border-radius: var(--border-radius);
         box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
-        animation: with .2s ease forwards;
+        animation: with .2s linear forwards;
         z-index: 999;
         background-color: #fff;
         .title{
@@ -87,10 +153,40 @@ export default {
         table{
             margin-top: 20px;
             width: 100%;
-            tr{
-                display: flex;
-                justify-content: space-around;
-                border-bottom: 1px solid #f2f2f2;
+            animation: tableShow .2s linear forwards;
+            box-sizing: border-box;
+            font-size: 14px;
+            color: #606266;
+            tbody{
+                .week{
+                    display: flex;
+                    width: 100%;
+                    border-bottom: 1px solid #f2f2f2;
+                    padding-bottom: 5px;
+                    margin-bottom: 10px;
+                    th{
+                        padding: 0;
+                        width: 14.28%
+                    }
+                }
+                .day{
+                    display: flex;
+                    flex-wrap: wrap;
+                    width: 100%;
+                    th{
+                        cursor: pointer;
+                        padding: 5px 0;
+                        width: 14.28%;
+                        &:hover{
+                            color: var(--theme-color)
+                        }
+                    }
+                    .is-today{
+                        background-color: var(--theme-color);
+                        color: #fff;
+                        border-radius: 4px;
+                    }
+                }
             }
         }
         .arrow{
@@ -109,8 +205,22 @@ export default {
         0%{
             height: 0;
         }
+        50%{
+            height: 50%;
+        }
         100%{
-            height: 200px;
+            height: 100%;
+        }
+    }
+    @keyframes tableShow {
+        0%{
+            opacity: 0;
+        }
+        50%{
+            opacity: 0.3;
+        }
+        100%{
+            opacity: 1;
         }
     }
 </style>
